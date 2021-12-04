19f34ef9d681f6a3c43c3b2f44bfdf6982497f94
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-12-03 23:18:30 -0500

    Fix eslint react errors 7

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -12,16 +12,17 @@ class Screen extends React.Component {
     const total = this.props.display.total ? this.props.display.total : '0';
 
     return (
-      <div className="screen"> <span>{this.props.display.operation ? this.props.display.operation : ''}</span> {this.props.display.next ? this.props.display.next : total }</div>
+      <div className="screen"> <div>{this.props.display.operation ? this.props.display.operation : ''}</div> {this.props.display.next ? this.props.display.next : total }</div>
     );
   }
 }
 
 Screen.propTypes = {
-  total: PropTypes.string.isRequired,
-  next: PropTypes.string.isRequired,
-  string: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  display: PropTypes.object,
+  total: PropTypes.string,
+  next: PropTypes.string,
+  string: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Screen;
